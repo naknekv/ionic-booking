62d95dcedf036e3ab3ff8c97263a3f8c2f5c0d9d
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-09-17 12:57:12 +0200

    Add logout button with redirect

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: "root",
@@ -6,11 +7,14 @@ import { Injectable } from "@angular/core";
 export class AuthService {
   private userIsAuthenticated = false;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   isAuthenticated = () => this.userIsAuthenticated;
 
   login = () => (this.userIsAuthenticated = true);
 
-  logout = () => (this.userIsAuthenticated = false);
+  logout = () => {
+    this.userIsAuthenticated = false;
+    this.router.navigateByUrl("");
+  };
 }
