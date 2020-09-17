82cc5b3bd445824fddc74c77e6bb57923dc23ea6
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-09-17 11:48:14 +0200

    Add offers page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Place } from "src/app/models/Place.model";
+import { PlacesService } from "../places.service";
 
 @Component({
-  selector: 'app-offers',
-  templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+  selector: "app-offers",
+  templateUrl: "./offers.page.html",
+  styleUrls: ["./offers.page.scss"],
 })
 export class OffersPage implements OnInit {
+  public offers: Place[];
 
-  constructor() { }
+  constructor(private placesService: PlacesService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewWillEnter() {
+    this.offers = this.placesService.getPlaces();
   }
-
 }
