5190c0ceca1f43e9b46d0bc9d0a9da0d860164a9
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-09-17 12:51:16 +0200

    Add AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./login/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "places/discover",
+    redirectTo: "login",
     pathMatch: "full",
   },
   {
@@ -16,11 +17,13 @@ const routes: Routes = [
     path: "places",
     loadChildren: () =>
       import("./places/places.module").then((m) => m.PlacesPageModule),
+    canLoad: [AuthGuard],
   },
   {
     path: "bookings",
     loadChildren: () =>
       import("./bookings/bookings.module").then((m) => m.BookingsPageModule),
+    canLoad: [AuthGuard],
   },
 ];
 
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "@ionic/angular";
+import { AuthService } from "./auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  selector: "app-login",
+  templateUrl: "./login.page.html",
+  styleUrls: ["./login.page.scss"],
 })
 export class LoginPage implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private navCtrl: NavController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  login = () => {
+    this.authService.login();
+    this.navCtrl.navigateForward("/places/discover");
+  };
 }
