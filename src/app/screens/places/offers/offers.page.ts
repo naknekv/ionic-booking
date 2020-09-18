cb3dc2e61acc365de5a6936b8b6148c678d550eb
Nacho Iacovino <nachoiacovino@berdac.com>
2020-09-18 13:04:23 +0200

    Add slideable item

--- src/app/screens/places/offers/offers.page.ts
+++ src/app/screens/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { IonItemSliding } from "@ionic/angular";
 import { Place } from "src/app/models/Place.model";
 import { PlacesService } from "../places.service";
 
@@ -10,11 +12,16 @@ import { PlacesService } from "../places.service";
 export class OffersPage implements OnInit {
   public offers: Place[];
 
-  constructor(private placesService: PlacesService) {}
+  constructor(private placesService: PlacesService, private router: Router) {}
 
   ngOnInit() {}
 
   ionViewWillEnter() {
     this.offers = this.placesService.getPlaces();
+  }
+
+  openEditPage(itemId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(["places/offers", itemId, "edit"]);
   }
 }
